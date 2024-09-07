58cfaaf06ae0967d9f1823f0eeaf51b7a0298a21
Michael D Havis <50241647+havis-michael@users.noreply.github.com>
2024-09-07 18:19:07 -0400

    feat: add new game button that creates squares
    Add the new game button that creates squares after prompting the user
    for the desired grid size. Maximum grid size allowed is 100 x 100.
    Zero and negative numbers are disallowed.

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+let newGameButton = document.querySelector(".new-game-button");
+const gameBoardWidth = 960;
+
+newGameButton.addEventListener("click", function () {
+  let gridWidth = prompt("How wide should the grid be?");
+  if (gridWidth === null || gridWidth < 1) {
+    return;
+  }
+
+  createSquares(gridWidth);
+});
+
+function createSquares(gridDimensions) {
+  if (gridDimensions > 100) {
+    alert("The maximum grid width allowed is 100.");
+    return;
+  }
+
+  let numberOfSquares = gridDimensions ** 2;
+  let squareSize = `${gameBoardWidth / gridDimensions}px`;
+  let squareContainer = document.querySelector(".square-hole");
+  squareContainer.innerHTML = "";
+
+  for (let i = 0; i < numberOfSquares; i++) {
+    let square = document.createElement("div");
+
+    square.setAttribute("class", "game-square");
+    square.style.width = squareSize;
+    square.style.height = squareSize;
+
+    squareContainer.appendChild(square);
+  }
+}
